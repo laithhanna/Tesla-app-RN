06ed15ee2672b82c78ee50bbd170ed9b0f5cf46d
Laith <laithhanna6@gmail.com>
2023-04-14 11:51:20 -0400

    Home screen header

---
+++ app/index.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}>My Model S</Text>
+          <Text style={styles.subtitle}>Parked</Text>
+        </View>
+        <FontAwesome name="user-circle" size={30} color="gray" />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#161818",
+  },
+  header: {
+    marginTop: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 24,
+    color: "#eee",
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  subtitle: {
+    color: "gray",
+    fontWeight: "500",
+  },
+});
