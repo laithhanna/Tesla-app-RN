aff9ebb9a43782087cb5728736b0582ffab7c5a4
Laith <laithhanna6@gmail.com>
2023-04-14 16:46:52 -0400

    implemented state to some of the controls icons

--- components/Controls.js
+++ components/Controls.js
@@ -1,17 +1,36 @@
-import { StyleSheet, View } from "react-native";
+import { Pressable, StyleSheet, View } from "react-native";
 import {
   Entypo,
   MaterialCommunityIcons,
   FontAwesome5,
   Ionicons,
 } from "@expo/vector-icons";
+import { useState } from "react";
 
 const Controls = () => {
+  const [lock, setLock] = useState(true);
+  const [boltState, setBoltState] = useState(false);
+
   return (
     <View style={styles.controls}>
-      <Entypo name="lock" size={26} color="gray" />
+      <Pressable onPress={() => setLock(!lock)}>
+        <Entypo
+          name={lock ? "lock" : "lock-open"}
+          size={26}
+          color={lock ? "gray" : "white"}
+        />
+      </Pressable>
+
       <MaterialCommunityIcons name="fan" size={26} color="gray" />
-      <FontAwesome5 name="bolt" size={26} color="gray" />
+
+      <Pressable onPress={() => setBoltState(!boltState)}>
+        <FontAwesome5
+          name="bolt"
+          size={26}
+          color={boltState ? "yellow" : "gray"}
+        />
+      </Pressable>
+
       <Ionicons name="car-sport-sharp" size={26} color="gray" />
     </View>
   );
